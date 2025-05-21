5fe2441ac654e48b77aae27df37f9f1d9780f53a
Dinh Hoang Phuc <98966221+DinhHoangPhuc@users.noreply.github.com>
2025-05-21 21:14:23 +0700

    add search filter and category ui

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,15 +1,49 @@
 import React from 'react';
 import { Navbar } from './navbar';
 import { Footer } from './footer';
+import { SearchFilters } from './search-filters';
+
+import { getPayload } from "payload";
+import configPromise from "@payload-config";
+import { Category } from '@/payload-types';
 
 interface Props {
     children: React.ReactNode;
 };
 
-function layout({ children }: Props) {
+const layout = async({ children }: Props) => {
+    const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: "categories",
+    depth: 1,
+    pagination: false,
+    where: {
+      parent: {
+        exists: false,
+      },
+    },
+  });
+
+  const formatedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((subdoc) => ({
+      ...(subdoc as Category),
+      subcategories: undefined,
+    })),
+  }));
+
+  console.log({
+    data,
+    formatedData,
+  });
+
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar/>
+            <SearchFilters data={formatedData}/>
             <div className="flex-1 bg-[#F4F4F0]">
                 {children}
             </div>
